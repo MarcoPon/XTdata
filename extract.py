06b6a064358fdd83518aa41d00216591af08ef1c
Marco Pontello <marcopon@gmail.com>
2016-02-29 15:13:22 +0100

    Now use a sliding windows for the last n blocks. Fixed formatting a bit.

--- extract.py
+++ extract.py
@@ -5,8 +5,7 @@
 debuglogfilename = r"p:\bitcoin-full\debug.log"
 csvfilename = r"graph.csv"
 resultsfilename = r"results.txt"
-#use this to start from a certain block...
-startfromhash = "00000000000000000066cb8b7bbaf5eaa14f61fc146b9ef08cd23ad35ed73ec8"
+windowsize = 288 #last n blocks to consider (144 = ~1 day)
 
 import re
 
@@ -15,47 +14,47 @@ blocksnum = 0
 totblocksize = 0
 totxtsize = 0
 
-calc = False
-if startfromhash == "":
-    calc = True
+blocksdata = []
+with open(debuglogfilename, "r") as fin:
+    for text in fin:
+        if "compression" in text:
+            res = re.findall(blocksre, text)[0]
+            blockhash = res[0]
+            blocksize = int(res[1])
+            blockcomp = int(res[2])
+            print text.strip()
+            blocksdata.append(( blockhash, blocksize, blockcomp ))
 
-with open(csvfilename, "w") as fout:
-    fout.write('"Block Size","XT Size"\n')
-    with open(debuglogfilename, "r") as fin:
-        for text in fin:
-            if "compression" in text:
-                res = re.findall(blocksre, text)[0]
-                blockhash = res[0]
-                blocksize = int(res[1])
-                blockcomp = int(res[2])
-                if calc == False:
-                    if blockhash == startfromhash:
-                        calc = True
-                if calc == True:
-                    print text.strip()
-                    blocksnum += 1
-                    totblocksize += blocksize
-                    totxtsize += blockcomp
-                    blockratio = float(blocksize) / blockcomp
-                    print "(Size %i, compressed %i, ratio %.2f)" % (blocksize, blockcomp,
-                                                             blockratio)
-                    fout.write("%i, %i\n" % (blocksize, blockcomp))
+if windowsize > 0:
+    blocksdata = blocksdata[-windowsize:]
+
+with open(csvfilename, "w") as fcsv:
+    fcsv.write('"Block Size","XT Size"\n')
+    blocksnum = len(blocksdata)
+    for block in blocksdata:                
+        blocksize = block[1]
+        blockcomp = block[2]
+        totblocksize += blocksize
+        totxtsize += blockcomp
+        blockratio = float(blocksize) / blockcomp
+        print "(Size %i, compressed %i, ratio %.2f)" % (blocksize, blockcomp,
+                                                 blockratio)
+        fcsv.write("%i, %i\n" % (blocksize, blockcomp))
 
 if blocksnum > 0:
-
     text = ""
-    text += "Blocks        : %i\n" % ( blocksnum )
-    text += "Tot block size: %s\n" % ( "{:,}".format(totblocksize) )
-    text += "Tot XT size   : %s\n" % ( "{:,}".format(totxtsize) )
-    text += "Average blocksize: %s\n" % ( "{:,}".format(totblocksize / blocksnum) )
-    text += "Average XT size  : %s\n" % ( "{:,}".format(totxtsize / blocksnum) )
-    text += "Ratio         : %.2f\n" % ( float(totblocksize) / totxtsize )
-    text += "Compression   : %.2f%% \n" % ( (totblocksize - totxtsize) * 100.0
+    text += "Blocks: %i\n" % ( blocksnum )
+    text += "Tot block size   : %s\n" % ( "{:>12,}".format(totblocksize) )
+    text += "Tot XT size      : %s\n" % ( "{:>12,}".format(totxtsize) )
+    text += "Average blocksize: %s\n" % ( "{:>12,}".format(totblocksize / blocksnum) )
+    text += "Average XT size  : %s\n" % ( "{:>12,}".format(totxtsize / blocksnum) )
+    text += "Ratio      : %.2f\n" % ( float(totblocksize) / totxtsize )
+    text += "Compression: %.2f%% \n" % ( (totblocksize - totxtsize) * 100.0
                                       / totblocksize )
     print
     print text
 
-    with open(resultsfilename, "w") as fout:
-        fout.write(text)
+    with open(resultsfilename, "w") as fres:
+        fres.write(text)
 
     
